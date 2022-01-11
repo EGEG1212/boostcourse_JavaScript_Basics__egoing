8815c3ed14d8fd4690d155107bea643bccc752c5
egeg1212 <dana1anne@naver.com>
2022-01-12 00:18:00 +0900

    으어어@_@어렵

--- 220110_02-class.js
+++ 220110_02-class.js
@@ -4,22 +4,34 @@ class Person{
         this.first = first;
         this.second = second;
     }
+    sum(){
+        return 'prototype : ' + (this.first+this.second);
+    }
 }
 
 
 var kim = new Person('kim', 10, 20);
-console.log('kim', kim);
+kim.sum = function(){
+    return 'this : ' + (this.first+this.second);
+}
+
+var lee = new Person('lee', 10, 10);
+console.log("kim.sum()", kim.sum());
+console.log("lee.sum()", lee.sum());
 
 
 
+// 😨뭔솔...😨
+// 🎈prototype
+// constructor로 생성된 모든 객체가 공통적으로 사용하는 속성과 함수를 만들고 싶을 때 사용.
+// prototype에서 함수를 정의하면 constructor가 실행 될 때 마다 함수가 생성되지 않고, 한 번만 정의되기 떄문에 메모리를 절약할 수 있다.
+// prototype에 sum이라는 함수가 있더라도 
+// 객체에 sum함수를 정의하면 객체에 지정된 함수가 호출된다.
 
+// 🎈new constructor 
+// 객체가 만들어지기 직전에 실행됨.
+// 객체의 초기상태를 지정하기 위한 함수의 이름
 
-// new생성자 함수(constructor)가 하는 일
-//  객체를 만든다.
-// 객체의 초기 상태를 정의한다.
-
-// ----------------------
-// 객체가 생성될때 function함수 안쓰고 constructor함수를 클래스 내에서 구현.
-
-// 자바스크립트는 객체를 생성할때 자동으로 constructor 함수를 호출합니다.
-// 객체의 초기값 설정
+// 🎈class
+// 객체를 찍어내는 기능
+// class안의 constructor 최초호출=초기값설정
